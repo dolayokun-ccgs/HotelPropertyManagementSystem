2a501040ab543c3f4a7596ab221bba29fedcc147
dolayokun-ccgs <159705460+dolayokun-ccgs@users.noreply.github.com>
2026-06-24 16:03:06 +0100

    Refactor payload cleansing to use process_payload

--- function_app.py
+++ function_app.py
@@ -1,32 +1,47 @@
 import json
 import logging
+
 import azure.functions as func
 
-from regex_cleaner import cleanse_payload
+from regex_cleaner import process_payload
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+app = func.FunctionApp()
 
 
-@app.route(route="cleanse")
+@app.function_name(name="RegexCleaner")
+@app.route(
+    route="cleanse",
+    auth_level=func.AuthLevel.FUNCTION
+)
 def cleanse(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
 
         payload = req.get_json()
 
-        cleaned_payload = cleanse_payload(payload)
+        cleaned_payload = process_payload(payload)
 
         return func.HttpResponse(
-            body=json.dumps(cleaned_payload),
+            body=json.dumps(
+                cleaned_payload,
+                ensure_ascii=False
+            ),
             mimetype="application/json",
             status_code=200
         )
 
     except Exception as ex:
 
-        logging.exception("Payload cleansing failed")
+        logging.exception(
+            "Payload cleansing failed"
+        )
 
         return func.HttpResponse(
-            body=str(ex),
+            body=json.dumps(
+                {
+                    "error": str(ex)
+                }
+            ),
+            mimetype="application/json",
             status_code=500
         )
